dba28203afcd4a77b3de831b936035966f429cd7
Melissa Wang <melissa.wang21@gmail.com>
2024-09-18 20:19:23 -0400

    cleaned up code

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -18,12 +18,6 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        {/* <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select> */}
         <Filter onCategoryChange={handleCategoryChange}/>
       </div>
       <ul className="Items">
